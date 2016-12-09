5a77309f849024728c8b7eb7f088c951b357dbfa
koi8-r <Burchenya_VP@murmangaz.ru>
2016-12-09 13:18:54 +0300

    'uglify'

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
+var webpack = require('webpack') ;
+
 module.exports = {
     entry: {
         main:       "./main",
+        'main.min': "./main",
         test:       "./test"
     },
     output: {
         path: './dist',
         filename: "[name].js" // name replace with entry value
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            compress: {
+                warnings: false
+            }
+        })
+    ]
 } ;
